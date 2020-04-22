78500267c259de8ee6e197f3b9d32a6e08d40a54
Alex Boeger <alexboeger@gmail.com>
2020-04-22 10:41:11 -0500

    Updated banner styling, Remove Insurance Template Content Right, Updated acf-fields as per new Insurance Template

--- page-templates/single-insurance.php
+++ page-templates/single-insurance.php
@@ -34,64 +34,55 @@ Template Post Type: insurance
 
 
 <!--- Main Content --->
-		<div id="insurance-content-container"class="container">
-			<section class="page-wrap">
-				<div class="row">
-				<div class="col-3">
-					<div class="wrapper">
-					    <!-- Insurance Menu -->
-						<?php
-						if(has_category('personal')) {
-	  							wp_nav_menu( array(
-	  							'theme_location' 	=> 'personal-insurance-menu',
-	  							'depth'           	=> 2,
-	  							'container'     	=> 'div',
-	  							'container_class' 	=> 'tree',
-	  							 //'walker'			=> new Nfr_Menu_Walker,
-	  							) );
+<div id="insurance-content-container"class="container">
+	<section class="page-wrap">
+		<div class="row">
+			<div class="col-3">
+				<div class="wrapper">
+				    <!-- Insurance Menu -->
+					<?php
+					if(has_category('personal')) {
+  							wp_nav_menu( array(
+  							'theme_location' 	=> 'personal-insurance-menu',
+  							'depth'           	=> 2,
+  							'container'     	=> 'div',
+  							'container_class' 	=> 'tree',
+  							 //'walker'			=> new Nfr_Menu_Walker,
+  							) );
 
-						} else(has_category('commercial')) {
-							  wp_nav_menu( array(
-							  'theme_location' 	=> 'commercial-insurance-menu',
-							  'depth'           	=> 2,
-							  'container'     	=> 'div',
-							  'container_class' 	=> 'tree',
-							   //'walker'			=> new Nfr_Menu_Walker,
-						   ) )
+					} else(has_category('commercial')) {
+						  wp_nav_menu( array(
+						  'theme_location' 	=> 'commercial-insurance-menu',
+						  'depth'           	=> 2,
+						  'container'     	=> 'div',
+						  'container_class' 	=> 'tree',
+						   //'walker'			=> new Nfr_Menu_Walker,
+					   ) )
 
-					   } ?>
-					</div>
-				</div>
-				<div class="col-9 insurance-content-right">
-					<?php the_field('content_right'); ?>
+				   } ?>
 				</div>
 			</div>
-		</section>
-		<section class="content-main">
-			<div class="row">
-				<div class="col-sm-12">
-					<?php
+			<div class="col-9 insurance-content-right">
+				<?php
 					// Start the loop.
 					while ( have_posts() ) : the_post();
 
-						// Include the page content template.
-						get_template_part( 'loop-templates/content', 'single' );
+					// Include the page content template.
+					get_template_part( 'loop-templates/content', 'single' );
 
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
 					// End the loop.
 					endwhile;
-					?>
-
-				</div>
-
+				?>
 			</div>
-		</section>
-	</div>
+		</div>
+	</section>
 </div>
+
 
 <!--- Insurance Footer --->
 <?php $image = get_field('footer_image'); ?>
